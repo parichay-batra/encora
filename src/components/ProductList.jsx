c97ab493f677311431350aa1a7edbda71a461495
parichay-batra <parichay.batra@mylofamily.com>
2024-07-31 10:05:58 +0530

    commit

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,29 +1,45 @@
-import { useEffect, useState } from "react";
+import { useReducer, useEffect } from "react";
 import { PRODUCT_DATA } from "../data/productData";
 
-const ProductList = () => {
-  const [productData, setProductData] = useState(PRODUCT_DATA);
-  const [selectedIndex, changeSelectedIndex] = useState(0);
+// Define the initial state
+const initialState = {
+  productData: PRODUCT_DATA,
+  selectedIndex: 0,
+};
 
-  const handleChangeVariant = (variant, index) => {
-    const data = [...productData];
-    data.forEach((product, productIndex) => {
-      product.variants.forEach((item, index) => {
-        if (item.sku_id === variant.sku_id) {
-          const selectedElement = product.variants.splice(index, 1);
-          product.variants.unshift(selectedElement[0]);
-        }
+// Define the reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_VARIANT":
+      const updatedData = [...state.productData];
+      updatedData.forEach((product) => {
+        product.variants.forEach((item, index) => {
+          if (item.sku_id === action.payload.sku_id) {
+            const selectedElement = product.variants.splice(index, 1);
+            product.variants.unshift(selectedElement[0]);
+          }
+        });
       });
-    });
-    console.log({ data });
+      return {
+        ...state,
+        productData: updatedData,
+      };
+    default:
+      return state;
+  }
+};
 
-    setProductData(data);
+const ProductList = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChangeVariant = (variant) => {
+    dispatch({ type: "CHANGE_VARIANT", payload: variant });
   };
 
   useEffect(() => {
-    console.log(productData);
+    console.log(state.productData);
     debugger;
-  }, [productData]);
+  }, [state.productData]);
 
   return (
     <div className="p-4 m-4">
@@ -31,17 +47,17 @@ const ProductList = () => {
         <h1 className="text-3xl font-bold underline">Product List</h1>
       </div>
       <div className="flex">
-        {productData.map((product, index) => {
+        {state.productData.map((product) => {
           return (
-            <div className="m-4 p-4  border border-black" key={product.item_id}>
-              <div>Name : {product.variants[0].sku_name}</div>
-              <div>Price : {product.variants[0].price}</div>
+            <div className="m-4 p-4 border border-black" key={product.item_id}>
+              <div>Name: {product.variants[0].sku_name}</div>
+              <div>Price: {product.variants[0].price}</div>
               <div>
-                Variants :
-                {product.variants.map((variant, index) => {
+                Variants:
+                {product.variants.map((variant) => {
                   return (
                     <div key={variant.sku_id}>
-                      <span onClick={() => handleChangeVariant(variant, index)}>
+                      <span onClick={() => handleChangeVariant(variant)}>
                         {variant.sku_name}
                       </span>
                     </div>
